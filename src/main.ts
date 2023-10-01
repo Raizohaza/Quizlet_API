11dffb997afca35f6aa954db94d158ade1bf2a5b
Raizohaza <toanphamhcmus@gmail.com>
2023-10-01 12:58:51 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Quizlet')
+    .setDescription('The Quizlet API description')
+    .setVersion('1.0')
+    .addTag('quizzes')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  const globalPrefix = 'api';
+  const port = process.env.PORT || process.argv[2] || 3333;
+
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
+}
+bootstrap();
